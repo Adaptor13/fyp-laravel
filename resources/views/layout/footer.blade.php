4bb4ef43cc74ee2a45a0f6983379976d0e6b8bba
Adaptor13 <justincheah13@gmail.com>
2025-03-05 22:50:20 +0800

    feat: add new files

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,26 @@
+<!-- Footer Section starts-->
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-9 col-12">
+                <ul class="footer-text">
+                    <li>
+                        <p class="mb-0">Copyright © <span id="year"></span> SentraD. All rights reserved.</p>
+                    </li>
+                    <li> <a href="#"> V1.0.0 </a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <ul class="footer-text text-end">
+                    <li> <a href="mailto:justincheah13@gmail.com."> Need Help <i class="ti ti-help"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section ends-->
+
+<script>
+    // JavaScript to set the current year dynamically
+    document.getElementById("year").textContent = new Date().getFullYear();
+</script>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::view('sign_in_1', 'sign_in_1')->name('sign_in_1');
